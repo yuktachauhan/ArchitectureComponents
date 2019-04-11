90ca17588e6b103316d76ad180725b4a5b2f2c81
yuktachauhan <yuktach20167@gmail.com>
2019-04-11 20:41:37 +0530

    Dao for notes

---
+++ app/src/main/java/com/example/android/notes/NoteDao.java
@@ -0,0 +1,32 @@
+package com.example.android.notes;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("DELETE FROM notes_table")    //to delete all notes from the table
+    void deleteCompleteNotes();
+
+    @Query("SELECT * FROM notes_table ORDER BY priority_column DESC") //* is used to select from all tables
+
+    LiveData<List<Note>>  getAllNotes(); //this method is used to represent the the data in recyclerview
+                                         //livedata is used to observe the data like if ther is any change in note_table,
+                                          //then this list will be automatically updated and our activity will be notified
+}
