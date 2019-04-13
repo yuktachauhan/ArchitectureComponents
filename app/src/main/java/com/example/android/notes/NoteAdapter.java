65bf7e2e6cfb3b7315e8926b5edda31ee3766ce6
yuktachauhan <yuktach20167@gmail.com>
2019-04-13 18:09:43 +0530

    RecyclerView for notes

---
+++ app/src/main/java/com/example/android/notes/NoteAdapter.java
@@ -0,0 +1,54 @@
+package com.example.android.notes;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+
+    private List<Note> notes = new ArrayList<Note>();
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view =LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.note_item,viewGroup,false);
+        return new NoteHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder noteHolder, int i) {
+       Note note =notes.get(i);
+       noteHolder.title.setText(note.getTitle());
+       noteHolder.description.setText(note.getDescription());
+       noteHolder.priority.setText(String.valueOf(note.getPriority()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> note){
+        this.notes=note;
+        notifyDataSetChanged();
+    }
+
+    public class NoteHolder extends RecyclerView.ViewHolder{
+        private TextView title,description,priority;
+
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.text_view_title);
+            description = itemView.findViewById(R.id.text_view_description);
+            priority = itemView.findViewById(R.id.text_view_priority);
+
+        }
+    }
+}
